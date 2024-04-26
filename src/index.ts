83bd984d18464fb4d18855ff3ff54302cd4e7a50
João Lucas <lucas.26.11.2014@gmail.com>
2024-04-26 14:58:35 -0300

    speed update

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,16 @@
-import p5 from 'p5';
-
 import { changeColor } from './utils/change-color';
 import { codeEffects } from './utils/code-effects';
-//import { dropThings } from './utils/drop-things';
-//import { flipEffect } from './utils/filp-effect';
+//import { dropThings } from './utils/drop-things';ss
 import { flipCard } from './utils/flip-card';
 import { lazyLoad } from './utils/lazy-load';
 import { letterEffect } from './utils/letter-effect';
 //import { pageLoad } from './utils/page-load';
-//import { scrollCar } from './utils/scroll-carr';
-//import { snakeAsset } from './utils/snake-asset';
 import { snakeGame } from './utils/snake-game';
 
 codeEffects();
 changeColor();
 snakeGame();
 lazyLoad();
-//snakeAsset();
-//pageLoad();
 //dropThings();
 letterEffect();
 flipCard();
-//flipEffect();
-//scrollCar();
